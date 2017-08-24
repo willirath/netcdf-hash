12abf46d6e1642638e4cecc6cab694046bfe9a44
Willi Rath <wrath@geomar.de>
2017-08-24 18:27:40 +0200

    Derive different versions of example data

--- update_example_data.sh
+++ update_example_data.sh
@@ -3,5 +3,11 @@
 urls=("https://www.unidata.ucar.edu/software/netcdf/examples/madis-sao.nc")
 
 for url in ${urls[*]}; do
-    curl ${url} -o example_data/"`basename ${url}`"
+    output_file=example_data/"`basename ${url}`"
+
+    curl ${url} -o ${output_file}
+
+    for kind in 3 4 6 7; do
+        nccopy -${kind} ${output_file} ${output_file}.${kind}
+    done
 done
